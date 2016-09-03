5d3006c408d790a2008550819c4919b4e913aa74
Lily He <yonglinlilyhe@gmail.com>
2016-09-02 22:38:13 -0400

    Removed right click context menu function and page

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.contextMenus.create({
-      title: "Locate this place",
-      id: "mapzen",
-      contexts: ["selection"]
-    });
-
-chrome.contextMenus.onClicked.addListener(function () {
-  chrome.tabs.create({url: "turn-by-turn.html"});
-});
